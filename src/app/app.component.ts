3782481d76498479ff11c00511fef5ca931eaae5
Perry Craft <ljcraft12@gmail.com>
2017-06-29 13:18:16 -0400

    finished product for angular animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,30 +10,44 @@ import { trigger, style, transition, animate, keyframes, query, stagger } from '
 
 //  Animations are always defined in the component decorator
   animations: [
-    trigger('listAnimations', [
+    // Trigger the animations for the list array
+    trigger('listAnimation', [
       // Go from any to any state
-     transition('* => *', [
-       // Define enter meaning on enter. then the style of the query. This style will be passed to the items in the items array
-       // inside the app.component.html interpolation.
-       query(':enter', style({opacity: 0 }), {optional: true}),
+      transition('* => *', [
+        // Define enter meaning on enter. then the style of the query. This style will be passed to the items in the items array
+        // inside the app.component.html interpolation.
+        query(':enter', style({opacity: 0 }), {optional: true}),
 
-       query(':enter', stagger('300ms', [
-         animate('1s ease-in', keyframes([
-           style({opacity: 0, transform: 'translateY(-75px)', offset: 0}),
-           style({opacity: .5, transform: 'translateY(35px)', offset: 0.3}),
-           style({opacity: 1, transform: 'translateY(0)', offset: 1})
-         ]))
-       ]),{optional: true}),
+        query(':enter', stagger('300ms', [
+          animate('1s ease-in', keyframes([
+            style({opacity: 0, transform: 'translateY(-75px)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)', offset: 0.3}),
+            style({opacity: 1, transform: 'translateY(0)', offset: 1})
+          ]))
+        ]),{optional: true}),
 
-       query(':leave', stagger('300ms', [
-         animate('1s ease-in', keyframes([
-           style({opacity: 1, transform: 'translateY(0)', offset: 0}),
-           style({opacity: .5, transform: 'translateY(35px)', offset: 0.3}),
-           style({opacity: 0, transform: 'translateY(-75px)', offset: 1})
-         ]))
-       ]),{optional: true}),
+        query(':leave', stagger('300ms', [
+          animate('1s ease-in', keyframes([
+            style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+            style({opacity: .5, transform: 'translateY(35px)', offset: 0.3}),
+            style({opacity: 0, transform: 'translateY(-75px)', offset: 1})
+          ]))
+        ]),{optional: true}),
 
-     ])
+      ])
+    ]),
+
+    // Trigger the animations for the paragraphs
+    trigger('explainerAnim', [
+      transition('* => *', [
+
+        query( '.col', style({ opacity: 0, transform: 'translateX(-40px' })),
+
+        query('.col', stagger('500ms', [
+          animate('800ms 1.2s ease-out', style({ opacity: 1, transform: 'translateX(0)'}))
+        ]))
+
+      ])
     ])
   ]
 })
